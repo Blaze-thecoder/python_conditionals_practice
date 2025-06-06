66f2f1d954b9e6ddaf7baef0a07de581e6993e85
BLAZE <yellowshadow338@gmail.com>
2025-06-06 11:52:25 +0530

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,23 @@
+# DISCLAIMER:
+# 🚨 THIS SCRIPT IS A JOKE 🚨
+# This program does not store or transmit any data.
+
+name = input("ENTER YOUR NAME : ")
+age = int(input("ENTER YOUR AGE : "))
+print(f"hello {name} your age is {age}")
+if(age > 18):
+    print("YOU ARE NOT ALLOWED")
+    exit()
+else:
+    print("YOU CAN PROCEED")
+
+print("welcome to main page !!!")  
+
+print("DONATE THE MONEY AND SAVE THE WORLD")
+card = input('''Enter the credit card no. (of any one FATHER/MOTHER — 
+                don't tell them that you're helping others — 
+                real heroes don't tell 😎)  ==>   ''')
+phone = input("ENTER THE MOBILE NO. which is linked to the credit card ==> ")
+otp = input('''LASTLY, JUST SEND THE OTP SECRETLY AND YOU WILL SAVE THE WORLD ==>  ''')
+
+print("MISSION PASSED !!! 🎉 (Just kidding, no money was taken)")
